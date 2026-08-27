5cc442632ce21397c3d0e9642c0263b292dc7f0c
mtb0x1 <mtb0x10x1@gmail.com>
2026-08-27 16:55:05 +0200

    feat(simd): init

--- src/common/math.rs
+++ src/common/math.rs
@@ -1,3 +1,5 @@
+use core::arch::wasm32::*;
+
 #[inline(always)]
 pub fn create_perspective_matrix(fov_y: f32, aspect: f32, near: f32, far: f32) -> [f32; 16] {
     let f = 1.0 / (fov_y / 2.0).tan();
@@ -23,6 +25,9 @@ pub fn create_perspective_matrix(fov_y: f32, aspect: f32, near: f32, far: f32) -
     ]
 }
 
+// @todo use of [f32; 4] the forth slot is padding,
+// this might yield a better wasm code using simd
+// https://rust.godbolt.org/z/sWGW7cq5s
 #[inline(always)]
 pub fn create_look_at_matrix(eye: [f32; 3], center: [f32; 3], up: [f32; 3]) -> [f32; 16] {
     let f = [center[0] - eye[0], center[1] - eye[1], center[2] - eye[2]];
@@ -54,15 +59,57 @@ pub fn create_look_at_matrix(eye: [f32; 3], center: [f32; 3], up: [f32; 3]) -> [
 
 #[inline(always)]
 pub fn multiply_matrices(a: &[f32; 16], b: &[f32; 16]) -> [f32; 16] {
-    let mut result = [0.0; 16];
-    for i in 0..4 {
-        for j in 0..4 {
-            let mut sum = 0.0;
-            for k in 0..4 {
-                sum += a[k * 4 + j] * b[i * 4 + k];
-            }
-            result[i * 4 + j] = sum;
+    // @todo remove the block if tests are okay
+    if false {
+        let mut r = [0.0; 16];
+
+        for col in 0..4 {
+            let b0 = b[col * 4];
+            let b1 = b[col * 4 + 1];
+            let b2 = b[col * 4 + 2];
+            let b3 = b[col * 4 + 3];
+
+            r[col * 4] = a[0] * b0 + a[4] * b1 + a[8] * b2 + a[12] * b3;
+            r[col * 4 + 1] = a[1] * b0 + a[5] * b1 + a[9] * b2 + a[13] * b3;
+            r[col * 4 + 2] = a[2] * b0 + a[6] * b1 + a[10] * b2 + a[14] * b3;
+            r[col * 4 + 3] = a[3] * b0 + a[7] * b1 + a[11] * b2 + a[15] * b3;
         }
+
+        r
+    } else {
+        // god this might not be safe, we livin on the Edge (not microsft :D) 
+        unsafe { *as_f32x16(&multiply_matrices_manual_simd(as_v128x4(a), as_v128x4(b))) }
     }
-    result
+}
+
+#[inline(always)]
+pub fn as_v128x4(m: &[f32; 16]) -> &[v128; 4] {
+    unsafe { core::mem::transmute::<&[f32; 16], &[v128; 4]>(m) }
+}
+
+pub fn as_f32x16(m: &[v128; 4]) -> &[f32; 16] {
+    unsafe { core::mem::transmute::<&[v128; 4], &[f32; 16]>(m) }
+}
+
+#[inline(always)]
+unsafe fn multiply_matrices_manual_simd(a: &[v128; 4], b: &[v128; 4]) -> [v128; 4] {
+    let mut out : [v128; 4] = [f32x4_splat(0.0); 4];
+
+    // @todo: use a fully unrolled code 
+    // i8x16_shuffle, f32x4_add, f32x4_mul ?
+    for i in 0..4 {
+        let x = f32x4_extract_lane::<0>(b[i]);
+        let y = f32x4_extract_lane::<1>(b[i]);
+        let z = f32x4_extract_lane::<2>(b[i]);
+        let w = f32x4_extract_lane::<3>(b[i]);
+
+        let mut r = f32x4_mul(a[0], f32x4_splat(x));
+        r = f32x4_add(r, f32x4_mul(a[1], f32x4_splat(y)));
+        r = f32x4_add(r, f32x4_mul(a[2], f32x4_splat(z)));
+        r = f32x4_add(r, f32x4_mul(a[3], f32x4_splat(w)));
+
+        out[i] = r;
+    }
+
+    out
 }
