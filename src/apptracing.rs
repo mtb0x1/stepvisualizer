c75776dd06213f579a4f921a49d0a3376485beac
mtb0x1 <mtb0x10x1@gmail.com>
2026-08-30 16:06:27 +0200

    fix: use caller provided it span names in tracing macro

--- src/apptracing.rs
+++ src/apptracing.rs
@@ -129,11 +129,9 @@ fn url_query_param(key: &str) -> Option<String> {
 #[macro_export]
 macro_rules! trace_span {
     ($name:expr) => {
-        let sp = tracing::span!(
-            tracing::Level::INFO,
-            $crate::common::constants::STEP_TRACER,
-            "{}",
-            $name
+        let sp = tracing::info_span!(
+            $name,
+            tracer = $crate::common::constants::STEP_TRACER
         );
         // don't use let _ =, it will drop the guard immediately
         let _entered = sp.entered();
